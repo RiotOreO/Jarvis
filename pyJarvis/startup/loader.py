d95be8b344364656e246cebfbecdef46d2e43a8e
btwOreO <andychris1808@gmail.com>
2024-10-31 17:03:47 +0530

    Update loader.py

--- pyJarvis/startup/loader.py
+++ pyJarvis/startup/loader.py
@@ -68,7 +68,7 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
             rmtree("addons")
         if not os.path.exists("addons"):
             subprocess.run(
-                f"git clone -q -b {Repo().active_branch} https://github.com/btworeo/JarvisAddons.git addons",
+                f"git clone -q -b termux https://github.com/btworeo/JarvisAddons.git addons",
                 shell=True,
             )
         else:
@@ -76,7 +76,7 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
 
         if not os.path.exists("addons"):
             subprocess.run(
-                "git clone -q https://github.com/btworeo/JarvisAddons.git addons",
+                "git clone -q -b termux https://github.com/btworeo/JarvisAddons.git addons",
                 shell=True,
             )
         if os.path.exists("addons/addons.txt"):
